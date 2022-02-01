e3bd20d81de5d9e8830e1276365ebe1281250b52
Beatrice Kulcsar <beatricekulcsar@MacBook-Pro-von-Beatrice.local>
2022-02-01 22:34:24 +0100

    add initial markup, toggle button functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [isChecked, setIsChecked ]= useState(false);
+
+  const price = isChecked?"16":"14";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Simple, traffic-based pricing</h1>
+       <p>Sign-up for our 30-day trial. No credit card required.</p>
+       <div className="container">
+          <div>
+            <p><span>100</span> PAGEVIEWS</p>
+            <p>${price} /month</p>
+          </div>
+          <input type="range" id="price"
+         min="1" max="5"></input>
+          <div>
+            <button id="btn-month" className="btn-billing" onClick={() => setIsChecked(false)}>Monthly Billing</button>
+            <input type="checkbox" id="billing-toggle" onClick={() => setIsChecked(!isChecked)} checked={isChecked}></input>
+            <button id="btn-year" className="btn-billing" onClick={() => setIsChecked(true)}>Yearly Billing</button>
+            <span>25% discount</span>
+          </div>
+          <div>
+            <ul>
+              <li>Unlimited websites</li>
+              <li>100% data ownership</li>
+              <li>Email reports</li>
+            </ul>
+            <button id="btn-trial">Start my trial</button>
+          </div>
+       </div>
     </div>
   );
 }
